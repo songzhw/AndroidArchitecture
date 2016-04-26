2699b723f3c6c11cab32db64015e742c9a052000
songzhw <songzhw2012@gmail.com>
2016-04-26 11:35:58 -0400

    init the mvp project

--- GoogleArchitecture/app/src/main/java/com/sample/MainActivity.java
+++ GoogleArchitecture/app/src/main/java/com/sample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.sample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.sample.mvp_basic.tasks.TasksActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickMainMvp(View v){
+        startActivity(new Intent(this, TasksActivity.class));
+    }
+
+    public void onClickMainMvpLoader(View v){
+//        startActivity(new Intent(this, TasksActivity.class));
+    }
+
+    public void onClickMainMvpBinding(View v){
+//        startActivity(new Intent(this, TasksActivity.class));
+    }
+
 }
