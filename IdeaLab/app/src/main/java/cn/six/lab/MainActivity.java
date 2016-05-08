2ffe930dd273f746729cb645f1739d7cc0631116
songzhw <songzhw2012@gmail.com>
2016-05-08 11:33:09 -0400

    OkHttp3 : cache

--- IdeaLab/app/src/main/java/cn/six/lab/MainActivity.java
+++ IdeaLab/app/src/main/java/cn/six/lab/MainActivity.java
@@ -6,13 +6,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import cn.six.lab.http.SimpleInterceptor;
+import okhttp3.Cache;
+import okhttp3.CacheControl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -30,13 +35,15 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                HttpAsyncTask task = new HttpAsyncTask();
                 task.execute();
             }
         });
 
+
     }
 
-    private AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+    private class HttpAsyncTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
             try {
@@ -46,19 +53,34 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
-    };
+    }
+
 
     private void startHttp() throws IOException {
+        File cachedDir = new File(this.getApplication().getExternalCacheDir(), "HttpCache");
+        Cache cache = new Cache(cachedDir, 10240);
+
         OkHttpClient okhttp = new OkHttpClient.Builder()
                 .addInterceptor(new SimpleInterceptor())
+                .cache(cache)
                 .build();
         Request req = new Request.Builder()
+                .cacheControl(new CacheControl.Builder().maxStale(30, TimeUnit.SECONDS).build())
                 .url("http://www.publicobject.com/helloworld.txt")
                 .header("User-Agent", "OkHttp Example")
                 .build();
+
+
         Response resp = okhttp.newCall(req)
                 .execute();
-        resp.body().close();
+        if(resp.code() != 504) {
+            // resource was cached. Show it
+            Log.d("szw", "cached!");
+        } else {
+            // resource was not cached.
+            Log.d("szw", "not cached!");
+        }
+
     }
 
 
