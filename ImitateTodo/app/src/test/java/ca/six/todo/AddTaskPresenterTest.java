072c1075c80dd33adc3b6e2131722e1b7a3c4c0a
songzhw <songzhw2012@gmail.com>
2016-05-16 09:02:25 -0400

    add presents for presenter

---
+++ ImitateTodo/app/src/test/java/ca/six/todo/AddTaskPresenterTest.java
@@ -0,0 +1,48 @@
+package ca.six.todo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.configuration.MockAnnotationProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.six.todo.add_task.AddTaskActivity;
+import ca.six.todo.add_task.AddTaskPresenter;
+import ca.six.todo.model.ITaskManager;
+import ca.six.todo.model.Task;
+
+/**
+ * Created by songzhw on 2016-05-16.
+ */
+public class AddTaskPresenterTest {
+    private List<Task> data;
+    private AddTaskPresenter presenter;
+    @Mock
+    public AddTaskActivity view;
+    @Mock
+    public ITaskManager model;
+
+    @Before
+    public void setUp(){
+        data = new ArrayList<>();
+        data.add(new Task("first"));
+        data.add(new Task("task two"));
+
+        MockitoAnnotations.initMocks(this);
+        presenter = new AddTaskPresenter(view);
+        presenter.model = model;
+
+        Mockito.when(model.getTasks()).thenReturn(data);
+    }
+
+    @Test
+    public void testAdd(){
+        presenter.addTask("third");
+        Mockito.verify(model).saveTask("third");
+        Mockito.verify(view).finishAdd();
+    }
+}
