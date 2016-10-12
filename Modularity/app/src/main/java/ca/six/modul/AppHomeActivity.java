6cadd3a637ec63f38e916ea5f8507c06efa90214
songzhw <songzhw2012@gmail.com>
2016-10-11 21:10:15 -0400

    init the repo

---
+++ Modularity/app/src/main/java/ca/six/modul/AppHomeActivity.java
@@ -0,0 +1,13 @@
+package ca.six.modul;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_home);
+    }
+}
