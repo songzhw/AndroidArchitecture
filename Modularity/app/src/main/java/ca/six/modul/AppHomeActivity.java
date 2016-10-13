01d1b7142c3796ee2a6857987ff6744451dd5bf1
songzhw <songzhw2012@gmail.com>
2016-10-12 22:19:15 -0400

    has a problem: app module doesn't know the activity in biz modules, then how can I jump to this activity?

--- Modularity/app/src/main/java/ca/six/modul/AppHomeActivity.java
+++ Modularity/app/src/main/java/ca/six/modul/AppHomeActivity.java
@@ -1,13 +1,48 @@
 package ca.six.modul;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class AppHomeActivity extends AppCompatActivity {
+import ca.six.common.LoginActivity;
+import ca.six.common.Session;
+
+public class AppHomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_home);
+
+        findViewById(R.id.tvHomeStock).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.tvHomeStock:
+                if(Session.getInstance().user == null){
+                    Intent it = new Intent(this, LoginActivity.class);
+                    startActivityForResult(it, 11);
+                }
+                break;
+        }
+    }
+
+    private void jump2Stock(){
+        Intent it = new Intent(this, StockActivity.class);
+        startActivity(it);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK) {
+            switch(requestCode){
+                case 11:
+                    jump2Stock();
+                    break;
+            }
+        }
     }
 }
