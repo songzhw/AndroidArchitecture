03d7c8095cfa73bc7380b3df28965f2d3eda396c
songzhw <songzhw2012@gmail.com>
2016-12-05 20:59:15 -0500

    flavor dimension

---
+++ GradleDemo/app/src/armFree/java/ca/six/gradledemo/MainActivity.java
@@ -0,0 +1,15 @@
+package ca.six.gradledemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("szw : temp = "+Temp.Date);
+    }
+}
