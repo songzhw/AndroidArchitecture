40ab8c112d42545c6381cc4352a34288a0b07d1b
songzhw <songzhw2012@gmail.com>
2017-02-01 12:20:00 -0500

    EventBus : transfer data between modules

--- Modularity/biz1/src/main/java/ca/six/biz1/StockActivity.java
+++ Modularity/biz1/src/main/java/ca/six/biz1/StockActivity.java
@@ -3,18 +3,37 @@ package ca.six.biz1;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import ca.six.common.utils.JumpUtils;
+
 // option 1: check login here
 // option 2: check login in the AppHomeActivity.
-    // AppHomeActivity.onActivityResult() will jump to StockActivity again.
+// AppHomeActivity.onActivityResult() will jump to StockActivity again.
 // I think option2 is a better idea.
-public class StockActivity extends Activity {
+public class StockActivity extends Activity implements View.OnClickListener {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock);
+        tv = (TextView) findViewById(R.id.tvStock);
+        tv.setOnClickListener(this);
 
         String value = getIntent().getStringExtra("key");
         System.out.println("szw StockActivity : value = " + value);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        EventBus.getDefault().post("from stock " + System.currentTimeMillis());
+        this.finish();
     }
 }
--- Modularity/biz2/src/main/java/ca/six/biz2/OptionActivity.java
+++ Modularity/biz2/src/main/java/ca/six/biz2/OptionActivity.java
@@ -2,12 +2,44 @@ package ca.six.biz2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class OptionActivity extends AppCompatActivity {
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import ca.six.common.utils.JumpUtils;
+
+public class OptionActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
+
+        tv = (TextView) findViewById(R.id.tvHomeOption);
+        tv.setOnClickListener(this);
+
+        EventBus.getDefault().register(this);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        JumpUtils.jump2(this, "ca.six.biz1","StockActivity");
+    }
+
+    // get the value from StockActivity
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessage(String msg) {
+        tv.setText(msg);
+    }
+
 }
