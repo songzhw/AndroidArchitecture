4228277b9e24e955c6fcb88893e040bc90e0b074
songzhw <songzhw2012@gmail.com>
2017-02-18 12:38:35 -0500

    databinding --> MVVM (a natural evolution to MVVM)

--- DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/Binding01Activity.java
+++ DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/Binding01Activity.java
@@ -12,17 +12,14 @@ import ca.six.bindingdemo.databinding.ActivityBindingDemoSimpleBinding;
  * Created by songzhw on 2017-02-18
  */
 
-public class BindingDemo01 extends Activity {
+public class Binding01Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityBindingDemoSimpleBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_binding_demo_simple);
 
-        TmpUser user = new TmpUser("name2", "desp2", true);
-        Demo01Handler handler = new Demo01Handler();
-        binding.setUser(user);
-        binding.setHandler(handler);
+        Binding01ViewModel vm = new Binding01ViewModel(binding);
     }
 
 }
---
+++ DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/Binding01ViewModel.java
@@ -0,0 +1,25 @@
+package ca.six.bindingdemo.tmp;
+
+import android.view.View;
+
+import ca.six.bindingdemo.databinding.ActivityBindingDemoSimpleBinding;
+
+/**
+ * Created by songzhw on 2017-02-18
+ */
+
+public class Binding01ViewModel {
+
+    private TmpUser user;
+
+    public Binding01ViewModel(ActivityBindingDemoSimpleBinding binding) {
+        user = new TmpUser("name2", "desp2", true);
+        binding.setUser(user);
+        binding.setHandler(this);
+    }
+
+    public void click01(View v){
+        System.out.println("szw click01 : "+user.desp);
+    }
+
+}
