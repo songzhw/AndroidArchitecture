ae5542c22a8dd32b6a3b6695079b4ebb2c8c1410
songzhw <songzhw2012@gmail.com>
2017-02-18 10:28:23 -0500

    init repo

---
+++ DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/BindingDemo01.java
@@ -0,0 +1,21 @@
+package ca.six.bindingdemo.tmp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ca.six.bindingdemo.R;
+
+/**
+ * Created by songzhw on 2017-02-18
+ */
+
+public class BindingDemo01 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_binding_demo_simple);
+    }
+
+}
---
+++ DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/TmpUser.java
@@ -0,0 +1,11 @@
+package ca.six.bindingdemo.tmp;
+
+/**
+ * Created by songzhw on 2017-02-18
+ */
+
+public class TmpUser {
+    public String name;
+    public String desp;
+    public boolean isMale;
+}
