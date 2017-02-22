c1d25e7546b219c9d9ba5e71fc809fc2f3f7517b
songzhw <songzhw2012@gmail.com>
2017-02-21 19:46:59 -0500

    databinding

---
+++ DataBindingDemo/app/src/main/java/ca/six/bindingdemo/view/OneBindingViewHolder.java
@@ -0,0 +1,15 @@
+package ca.six.bindingdemo.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by songzhw on 2017-02-21
+ */
+
+public class OneBindingViewHolder extends RecyclerView.ViewHolder {
+
+    public OneBindingViewHolder(View itemView) {
+        super(itemView);
+    }
+}
