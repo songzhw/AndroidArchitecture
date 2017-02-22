fc98d7c8b11bee7bf98862399a52193cf7a3ab02
songzhw <songzhw2012@gmail.com>
2017-02-21 20:13:04 -0500

    databinding : Recycler with a common adapter

--- DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/Binding01Activity.java
+++ DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/Binding01Activity.java
@@ -4,9 +4,17 @@ package ca.six.bindingdemo.tmp;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.six.bindingdemo.BR;
 import ca.six.bindingdemo.R;
 import ca.six.bindingdemo.databinding.ActivityBindingDemoSimpleBinding;
+import ca.six.bindingdemo.tmp.rv.TmpItem;
+import ca.six.bindingdemo.view.OneBindingAdapter;
 
 /**
  * Created by songzhw on 2017-02-18
@@ -15,13 +23,28 @@ import ca.six.bindingdemo.databinding.ActivityBindingDemoSimpleBinding;
 public class Binding01Activity extends Activity {
 
     private Binding01ViewModel vm;
+    private ActivityBindingDemoSimpleBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityBindingDemoSimpleBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_binding_demo_simple);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_binding_demo_simple);
 
         vm = new Binding01ViewModel(binding);
+
+        initRecyclerView();
     }
 
+    private void initRecyclerView() {
+        List<TmpItem> data = new ArrayList<>();
+        for(int i = 0 ; i < 18; i++){
+            data.add(new TmpItem("item [ "+i+" ]", i+10));
+        }
+
+        OneBindingAdapter<TmpItem> adapter = new OneBindingAdapter<>(this, R.layout.item_rv, BR.item, data);
+        RecyclerView rv = binding.rvBindingDemo;
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(adapter);
+
+    }
 }
--- DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/Binding01ViewModel.java
+++ DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/Binding01ViewModel.java
@@ -4,6 +4,7 @@ import android.view.View;
 
 import ca.six.bindingdemo.BR;
 import ca.six.bindingdemo.databinding.ActivityBindingDemoSimpleBinding;
+import ca.six.bindingdemo.view.OneBindingAdapter;
 
 /**
  * Created by songzhw on 2017-02-18
@@ -21,20 +22,27 @@ public class Binding01ViewModel {
         binding.setHandler(this);
     }
 
+
     public void click01(View v) {
         System.out.println("szw click01 : " + user);
     }
 
-    // how to make UI change when I update the model
-    //      : 01. binding.invalidateAll()
-    //      : 02. Make TmepUser class a subclass of BaseObservable
-                  // (but when you check isMale, only show tvWonderWoman. The name is still the same)
-                  // (you may have to use setName(), not just user.name(). Then the name will change too)
+/*
+how to make UI change when I update the model
+   : 01. binding.invalidateAll()
+   : 02. Make TmepUser class a subclass of BaseObservable
+      (but when you check isMale, only show tvWonderWoman. The name is still the same)
+      (you may have to use setName(), not just user.name(). Then the name will change too)
+*/
     public void onChecked(boolean isChecked){
         user.setName(user.name + (isChecked?" Y ":" N "));
 //        binding.invalidateAll(); // show/disappear tvWonderWoman, and update the tvName
 //        binding.notifyPropertyChanged(BR.user); // does not work
         System.out.println("szw click02 : " + user);
     }
+
+
+
+
 
 }
