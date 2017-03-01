dff1c02513510b570a2daaeff616f90b07720779
songzhw <songzhw2012@gmail.com>
2017-02-28 20:52:17 -0500

    databinding : custom event/attribute

--- DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/iv/DataBindingAdapters.java
+++ DataBindingDemo/app/src/main/java/ca/six/bindingdemo/tmp/iv/DataBindingAdapters.java
@@ -4,13 +4,22 @@ import android.databinding.BindingAdapter;
 import android.support.annotation.DrawableRes;
 import android.widget.ImageView;
 
+import ca.six.bindingdemo.tmp.Binding01ViewModel;
+import ca.six.bindingdemo.tmp.custom.ICustomListener;
+import ca.six.bindingdemo.tmp.custom.MyTextView;
+
 /**
  * Created by songzhw on 2017-02-27
  */
 
 public class DataBindingAdapters {
     @BindingAdapter("android:src")
-    public static void setImage(ImageView iv, @DrawableRes int res){
+    public static void setImage(ImageView iv, @DrawableRes int res) {
         iv.setImageResource(res);
+    }
+
+    @BindingAdapter("android:onMyChange")
+    public static void setChangeListener(MyTextView view, ICustomListener model) {
+        view.setListener(model);
     }
 }
