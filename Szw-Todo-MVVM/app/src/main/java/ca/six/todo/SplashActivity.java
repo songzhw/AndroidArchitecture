bc70120525fe96aba36f927aafdd10fabf70d80e
songzhw <songzhw2012@gmail.com>
2017-03-04 01:28:55 -0500

    splash

--- Szw-Todo-MVVM/app/src/main/java/ca/six/todo/SplashActivity.java
+++ Szw-Todo-MVVM/app/src/main/java/ca/six/todo/SplashActivity.java
@@ -2,12 +2,19 @@ package ca.six.todo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
+// 本页不宜用android:noHistory. 因为要防止press home key
 public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
     }
+
+
 }
---
+++ Szw-Todo-MVVM/app/src/main/java/ca/six/todo/biz/tasks/TasksActivity.java
@@ -0,0 +1,10 @@
+package ca.six.todo.biz.tasks;
+
+import android.app.Activity;
+
+/**
+ * Created by songzhw on 2017-03-04
+ */
+
+public class TasksActivity extends Activity {
+}
