e878dfc4ba6dec17efc07069b725dbdee19f7b9a
songzhw <songzhw2012@gmail.com>
2017-05-07 18:40:45 -0400

    Add ui for AddTweet

--- SixClean/app/src/main/java/ca/six/clean/biz/add/AddTweetActivity.java
+++ SixClean/app/src/main/java/ca/six/clean/biz/add/AddTweetActivity.java
@@ -1,4 +1,48 @@
 package ca.six.clean.biz.add;
 
-public class AddTweetActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import ca.six.clean.R;
+import ca.six.clean.domain.gateway.FakeEntityGateway;
+import ca.six.clean.domain.usecase.AddTweetUseCase;
+
+public class AddTweetActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+
+    private CheckBox cb;
+    private EditText et;
+    private FloatingActionButton fab;
+    private AddTweetPresenter presenter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tweet);
+
+        AddTweetUseCase useCase = new AddTweetUseCase(new FakeEntityGateway());
+        presenter = new AddTweetPresenter(useCase);
+
+        cb = (CheckBox) findViewById(R.id.cbAddTweet);
+        cb.setOnCheckedChangeListener(this);
+        et = (EditText) findViewById(R.id.etAddTweet);
+        fab = (FloatingActionButton) findViewById(R.id.fabAddTweet);
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        presenter.save(et.getText().toString(), cb.isChecked());
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+    }
 }
