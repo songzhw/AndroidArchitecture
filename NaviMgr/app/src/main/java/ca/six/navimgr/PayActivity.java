8b9a58dc975c326bd483a71df2fd0934b4e6ab6f
songzhw <songzhw2012@gmail.com>
2018-03-19 17:01:17 -0400

    add all pages we need

---
+++ NaviMgr/app/src/main/java/ca/six/navimgr/PayActivity.java
@@ -0,0 +1,25 @@
+package ca.six.navimgr;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class PayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv_btn);
+    }
+
+    public void onClickSimpleButton(View v) {
+
+    }
+
+    public void onClickSimpleButton2(View v) {
+
+    }
+
+}
