462ad102e8540bcab4be40063a0845c68d7cef49
songzhw <songzhw2012@gmail.com>
2018-05-14 10:59:00 -0400

    fix the crashes

--- MVI/MVI101/MVI_Client/app/src/main/java/ca/six/mvi/biz/home/HomePresenter.kt
+++ MVI/MVI101/MVI_Client/app/src/main/java/ca/six/mvi/biz/home/HomePresenter.kt
@@ -7,11 +7,12 @@ import io.reactivex.schedulers.Schedulers
 class HomePresenter {
 
     fun onCreate(){
-        HttpEngine().get("http://localhost:8899/todos")
+        HttpEngine().get("http://192.168.2.26:8899/todos")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe{ resp ->
-                    println("presenter : $resp")
+                    println("szw presenter : $resp")
+                    println("szw p2 : ${resp.body()?.string()}")
                 }
     }
 }
