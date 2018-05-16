e4f357e27764dda54a9493aad554d4e3459be8eb
songzhw <songzhw2012@gmail.com>
2018-05-16 07:50:18 -0400

    mvi

--- MVI/MVI101/MVI_Client/app/src/main/java/ca/six/mvi/biz/home/HomeActivity.kt
+++ MVI/MVI101/MVI_Client/app/src/main/java/ca/six/mvi/biz/home/HomeActivity.kt
@@ -6,25 +6,28 @@ import ca.six.mvi.R
 import java.util.*
 
 class HomeActivity : AppCompatActivity() {
-    val presenter = HomePresenter()  //TODO has a chance to inject it ?!
+    lateinit var presenter : HomePresenter //TODO has a chance to inject it ?!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        presenter = HomePresenter(this)
         presenter.onCreate()
     }
 
 
-    fun render(error : Throwable) {
-
-    }
-
-    fun render(isLoading : Boolean) {
-
-    }
-
-    fun render(todoList : List<Todo>){
-
+    fun render(data : HomeData){
+        if(data.isLoading){
+            // show pb
+        } else if(data.isError){
+            // show error
+        } else if(data.todos != null){
+            if(data.todos?.size == 0){
+                // show empty view
+            } else {
+                // recyclerview
+            }
+        }
     }
 }
--- MVI/MVI101/MVI_Client/app/src/main/java/ca/six/mvi/biz/home/HomePresenter.kt
+++ MVI/MVI101/MVI_Client/app/src/main/java/ca/six/mvi/biz/home/HomePresenter.kt
@@ -3,16 +3,14 @@ package ca.six.mvi.biz.home
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
-class HomePresenter {
+class HomePresenter(val view : HomeActivity) {
     val model = HomeModel()
 
     fun onCreate() {
         model.getTodoList()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { data ->
-                    println("szw presenter ${data.toString()}")
-                }
+                .subscribe(view::render)
 
     }
 }
