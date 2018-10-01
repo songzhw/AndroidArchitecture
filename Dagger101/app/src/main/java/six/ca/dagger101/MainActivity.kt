f788ad489cce429bd724703f6ee95c4cb1ffe650
songzhw <songzhw2012@gmail.com>
2018-09-30 20:17:39 -0400

    init the repo

---
+++ Dagger101/app/src/main/java/six/ca/dagger101/MainActivity.kt
@@ -0,0 +1,12 @@
+package six.ca.dagger101
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
