9e7776afd850d0309660d4aaaf319e20671ff7b0
songzhw <songzhw2012@gmail.com>
2018-10-07 18:39:25 -0400

    refactor

--- Tools/RxJavaPalyGround/app/src/main/java/ca/six/demo/util/Navigation.kt
+++
@@ -1,9 +0,0 @@
-package ca.six.demo.util
-
-import android.app.Activity
-import android.content.Intent
-import kotlin.reflect.KClass
-
-fun Activity.nav(clazz: Class<out Activity>){
-    startActivity(Intent(this, clazz))
-}
