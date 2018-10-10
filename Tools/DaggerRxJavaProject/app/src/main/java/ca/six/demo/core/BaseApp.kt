7eea60835d9dd1ee5334981935a73738b7ec5d45
songzhw <songzhw2012@gmail.com>
2018-10-09 20:42:20 -0400

    singleton retrofit client

--- Tools/DaggerRxJavaProject/app/src/main/java/ca/six/demo/core/BaseApp.kt
+++ Tools/DaggerRxJavaProject/app/src/main/java/ca/six/demo/core/BaseApp.kt
@@ -4,16 +4,22 @@ import android.app.Application
 import ca.six.demo.core.di.AppComponent
 import ca.six.demo.core.di.AppModule
 import ca.six.demo.core.di.DaggerAppComponent
+import ca.six.demo.core.di.HttpComponent
 
 class BaseApp : Application() {
 
     lateinit var appComponent: AppComponent
+    lateinit var httpComponent : HttpComponent
 
     override fun onCreate() {
         super.onCreate()
 
         appComponent = DaggerAppComponent.builder()
                 .appModule(AppModule(this))
+                .build()
+
+        httpComponent = appComponent.httpComponentBuilder()
+                .baseHttpUrl("https://api.github.com")
                 .build()
     }
 
