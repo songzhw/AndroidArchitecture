166f178cd019132aca125f91b1e898f87b9288de
songzhw <songzhw2012@gmail.com>
2018-10-23 13:00:21 -0400

    compiler error: error: [Dagger/MissingBinding] android.content.Context cannot be provided without an @Provides-annotated method.

--- Tools/DaggerPlayground/app/src/main/java/six/ca/dagger101/thirty2_test/DI32.kt
+++ Tools/DaggerPlayground/app/src/main/java/six/ca/dagger101/thirty2_test/DI32.kt
@@ -1,50 +1,44 @@
 package six.ca.dagger101.thirty2_test
 
+import android.content.Context
 import android.content.SharedPreferences
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.Retrofit
-import okhttp3.OkHttpClient
-import java.util.concurrent.TimeUnit
 import android.preference.PreferenceManager
+import dagger.BindsInstance
+import dagger.Component
+import dagger.Module
+import dagger.Provides
 
-class Repo{
-	var id : Int = 0 //lateinit var不能用于基本类型
-	var isForked = false
-	lateinit var name: String
-}
-
-interface UserApiService {
-	@GET("users/{user}/repos")
-	fun listRepos(@Path("user") user: String): Call<List<Repo>>
-}
-
-class UserManager(val sp : SharedPreferences, val api : UserApiService)
-
-class PasswordValidator
-
-class LoginPresenter(val mgr: UserManager, val validator: PasswordValidator)
-
-class LoginActivity : AppCompatActivity(){
-	private lateinit var presenter : LoginPresenter
-
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-
-		val okhttpClient = OkHttpClient.Builder()
-				.connectTimeout(30, TimeUnit.SECONDS)
-				.build()
-		val retrofit = Retrofit.Builder()
-				.client(okhttpClient)
-				.baseUrl("https://api.github.com")
-				.build()
+@Module
+class LoginModule {
+	@Provides
+	fun api(): UserApiService {
+		val retrofit = HttpEngine.retrofit()
 		val userApiService = retrofit.create(UserApiService::class.java)
-		val preferences = PreferenceManager.getDefaultSharedPreferences(this)
-		val userManager = UserManager(preferences, userApiService)
-		val passwordValidator = PasswordValidator()
-		presenter = LoginPresenter(userManager, passwordValidator)
+		return userApiService
 	}
+
+	@Provides
+	fun sp(ctx: Context): SharedPreferences {
+		return PreferenceManager.getDefaultSharedPreferences(ctx)
+	}
+
+	@Provides
+	fun mgr(sp: SharedPreferences, api: UserApiService): UserManager {
+		return UserManager(sp, api)
+	}
+
+	@Provides
+	fun validator(): PasswordValidator {
+		return PasswordValidator()
+	}
+
+	@Provides
+	fun presenter(mgr: UserManager, validator: PasswordValidator) : LoginPresenter {
+		return LoginPresenter(mgr, validator)
+	}
+}
+
+@Component(modules = [LoginModule::class])
+interface LoginComponent {
+	fun inject(activity: LoginActivity)
 }
