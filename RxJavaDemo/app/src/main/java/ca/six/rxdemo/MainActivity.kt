70d71923c348dbad56d23455493a53bd45edf3bf
songzhw <songzhw2012@gmail.com>
2019-04-02 16:21:52 -0400

    rxjava throttleFirst

--- RxJavaDemo/app/src/main/java/ca/six/rxdemo/MainActivity.kt
+++ RxJavaDemo/app/src/main/java/ca/six/rxdemo/MainActivity.kt
@@ -6,7 +6,6 @@ import android.view.View
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.properties.Delegates
@@ -22,14 +21,54 @@ class MainActivity : AppCompatActivity() {
   }
 
   private fun runRxDemo() {
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
-
-    disposable = Observable.interval(250, TimeUnit.MILLISECONDS)
+    disposable = Observable.interval(1000, TimeUnit.MILLISECONDS)
+      .doOnNext{num ->
+        val time = SimpleDateFormat("hh:mm:ss.SSS").format(Date())
+        println("szw map original = $num at $time")
+      }
+      .throttleFirst(3000, TimeUnit.MILLISECONDS)
       .subscribe { v ->
-        val time = SimpleDateFormat("hh:mm:ss").format(Date())
-        println("szw ${v}, at ${time}")
+        val time = SimpleDateFormat("hh:mm:ss.SSS").format(Date())
+        println("    szw receives ${v}, at ${time}")
       }
   }
+/*
+没有throttle时, interval()数据是从0开始的
+
+
+throttleFirst:
+throttleLast: 1, 4, 7, ... 即时间点到了之后, 接下一来的第一个事件
+throttleLatest
+
+ */
+
+/*
+throttleFirst:
+szw map original = 0 at 04:20:59.377
+    szw receives 0, at 04:20:59.378
+szw map original = 1 at 04:21:00.373
+szw map original = 2 at 04:21:01.375
+szw map original = 3 at 04:21:02.375
+szw map original = 4 at 04:21:03.373
+    szw receives 4, at 04:21:03.373
+szw map original = 5 at 04:21:04.375
+szw map original = 6 at 04:21:05.374
+szw map original = 7 at 04:21:06.374
+szw map original = 8 at 04:21:07.373
+    szw receives 8, at 04:21:07.375
+szw map original = 9 at 04:21:08.373
+szw map original = 10 at 04:21:09.374
+szw map original = 11 at 04:21:10.374
+szw map original = 12 at 04:21:11.377
+    szw receives 12, at 04:21:11.381
+ */
+
+
+/*
+throttleLast
+
+ */
+
 
   fun merge() {
     val sdf = SimpleDateFormat("hh:mm:ss")
