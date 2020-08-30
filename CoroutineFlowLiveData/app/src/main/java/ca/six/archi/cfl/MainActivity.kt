80169bd3f0d1408f943d048d5d6c3cddcdd7bfa2
songzhw <songzhw2012@gmail.com>
2020-08-30 16:49:27 -0400

    crash: lateinit property loginLiveData has not been initialized

--- CoroutineFlowLiveData/app/src/main/java/ca/six/archi/cfl/MainActivity.kt
+++ CoroutineFlowLiveData/app/src/main/java/ca/six/archi/cfl/MainActivity.kt
@@ -1,14 +1,32 @@
 package ca.six.archi.cfl
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
 import ca.six.archi.cfl.core.Http
+import ca.six.archi.cfl.core.LoginResponse
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Http.service.login();
+        val vm = ViewModelProvider(this).get(MainViewModel::class.java)
+        vm.loginLiveData.observe(this) { resp ->
+            println("szw result = $resp")
+        }
+    }
+}
+
+class MainViewModel : ViewModel() {
+    lateinit var loginLiveData: LiveData<LoginResponse>
+
+    fun login() {
+        loginLiveData = liveData {
+            Http.service.login()
+        }
     }
 }
