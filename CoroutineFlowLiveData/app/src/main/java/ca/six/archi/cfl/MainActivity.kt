193e9487276f35e08ecc6ec757a89e1ded8850b6
songzhw <songzhw2012@gmail.com>
2020-09-01 10:02:32 -0400

    add rv

--- CoroutineFlowLiveData/app/src/main/java/ca/six/archi/cfl/MainActivity.kt
+++ CoroutineFlowLiveData/app/src/main/java/ca/six/archi/cfl/MainActivity.kt
@@ -3,19 +3,42 @@ package ca.six.archi.cfl
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.*
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import ca.six.archi.cfl.core.Http
 import ca.six.archi.cfl.data.Plant
+import ca.six.oneadapter.lib.OneDiffAdapter
+import ca.six.oneadapter.lib.RvViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: OneDiffAdapter<Plant>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val diffCallback = object : DiffUtil.ItemCallback<Plant>() {
+            override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean = oldItem.plantId == newItem.plantId
+            override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean = oldItem == newItem
+        }
+
+        rv.layoutManager = GridLayoutManager(this, 3)
+        adapter = object : OneDiffAdapter<Plant>(diffCallback, R.layout.item_plants) {
+            override fun apply(vh: RvViewHolder, value: Plant, position: Int) {
+                //vh.setSrc(R.id.ivPlant, value.imageUrl)
+                vh.setText(R.id.tvPlant, value.name)
+            }
+
+        }
+        rv.adapter = adapter
+
+
         val vm = ViewModelProvider(this).get(MainViewModel::class.java)
-        vm.login().observe(this) { resp ->
-            println("szw result = $resp")
+        vm.getPlants().observe(this) { resp ->
             println("szw Actv: ${Thread.currentThread().name}") //=> main线程
+            adapter.refresh(resp)
         }
 
     }
@@ -23,7 +46,7 @@ class MainActivity : AppCompatActivity() {
 
 class MainViewModel : ViewModel() {
 
-    fun login(): LiveData<List<Plant>> {
+    fun getPlants(): LiveData<List<Plant>> {
         // liveData{}如果没有参数Dispatcher.IO的话, 那其lambda就是运行在主线程上!
         return liveData(Dispatchers.IO) {
             println("szw thread = ${Thread.currentThread().name}")
