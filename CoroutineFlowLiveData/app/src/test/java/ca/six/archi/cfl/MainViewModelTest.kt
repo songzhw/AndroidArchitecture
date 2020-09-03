5dc298229aeba8f36150d35666cb3ff7fdd85477
songzhw <songzhw2012@gmail.com>
2020-09-02 22:54:36 -0400

    fix the previous error in the MainLooper

--- CoroutineFlowLiveData/app/src/test/java/ca/six/archi/cfl/MainViewModelTest.kt
+++ CoroutineFlowLiveData/app/src/test/java/ca/six/archi/cfl/MainViewModelTest.kt
@@ -1,12 +1,22 @@
 package ca.six.archi.cfl
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import junit.framework.Assert.*
+import org.junit.Rule
 import org.junit.Test
 
 class MainViewModelTest {
+    @get:Rule var rule = InstantTaskExecutorRule()
+
     @Test
     fun updateDisplayThreeTimes_shouldFirstGridThenListThenGrid() {
         val vm = MainViewModel()
+        assertTrue(vm.isGrid)
+
+        vm.updateDisplay()
+        assertFalse(vm.isGrid)
+
+        vm.updateDisplay()
         assertTrue(vm.isGrid)
     }
 }
