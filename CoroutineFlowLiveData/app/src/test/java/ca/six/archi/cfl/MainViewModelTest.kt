49aed2aba0ca6649a75822b34a709c47715af111
songzhw <songzhw2012@gmail.com>
2020-09-03 10:51:34 -0400

    refactor the code

--- CoroutineFlowLiveData/app/src/test/java/ca/six/archi/cfl/MainViewModelTest.kt
+++ CoroutineFlowLiveData/app/src/test/java/ca/six/archi/cfl/MainViewModelTest.kt
@@ -1,14 +1,16 @@
 package ca.six.archi.cfl
 
-import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import ca.six.archi.cfl.core.DepProvider
+import ca.six.archi.cfl.core.Http
 import ca.six.archi.cfl.core.IHttpService
 import ca.six.archi.cfl.data.Plant
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.`when`
@@ -16,8 +18,40 @@ import org.mockito.Mockito.mock
 
 class MainViewModelTest {
     @get:Rule var rule1 = InstantTaskExecutorRule()
+
     @ExperimentalCoroutinesApi
     @get:Rule var rule2 = MainCoroutineRule()
+
+    @Before
+    fun setup() = runBlocking {
+        val data = arrayListOf(
+            Plant("1", "1", "1", 9),
+            Plant("2", "2", "2", 3),
+            Plant("3", "3", "3", 3),
+            Plant("5", "5", "5", 3)
+        )
+        val mockHttp = mock(IHttpService::class.java)
+        `when`(mockHttp.getAllPlants()).thenReturn(data)
+        DepProvider.http = mockHttp
+    }
+
+    @After
+    fun clear() {
+        DepProvider.http = Http.service
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun fetchPlants() {
+        val testDispatcher = TestCoroutineDispatcher()
+        val vm = MainViewModel()
+        vm.dispatch = testDispatcher
+        assertEquals(0, vm.data.size)
+
+        vm.fetchPlants()
+        assertEquals(4, vm.data.size)
+    }
+
 
     @Test
     fun updateDisplayThreeTimes_shouldFirstGridThenListThenGrid() {
@@ -52,26 +86,4 @@ class MainViewModelTest {
         assertEquals(4, vm.dataLiveData.value?.size)
     }
 
-    @ExperimentalCoroutinesApi
-    @Test
-    fun fetchPlants() = runBlocking{
-        //TODO 放before()去, 并记得清理
-        val data = arrayListOf(
-            Plant("1", "1", "1", 9),
-            Plant("2", "2", "2", 3),
-            Plant("3", "3", "3", 3),
-            Plant("5", "5", "5", 3)
-        )
-        val mockHttp = mock(IHttpService::class.java)
-        `when`(mockHttp.getAllPlants()).thenReturn(data)
-        DepProvider.http = mockHttp
-
-        val testDispatcher = TestCoroutineDispatcher()
-        val vm = MainViewModel()
-        vm.dispatch = testDispatcher
-        assertEquals(0, vm.data.size)
-
-        vm.fetchPlants()
-        assertEquals(4, vm.data.size)
-    }
 }
