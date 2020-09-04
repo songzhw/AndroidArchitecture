dde92044ed31681b9630700a45bc2b8891ec7b26
songzhw <songzhw2012@gmail.com>
2020-09-04 09:59:26 -0400

    making parcelable is much more easier for now

--- CoroutineFlowLiveData/app/src/main/java/ca/six/archi/cfl/data/Plant.kt
+++ CoroutineFlowLiveData/app/src/main/java/ca/six/archi/cfl/data/Plant.kt
@@ -1,5 +1,9 @@
 package ca.six.archi.cfl.data
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Plant(
     val plantId: String,
     val name: String,
@@ -7,6 +11,6 @@ data class Plant(
     val growZoneNumber: Int,
     val wateringInterval: Int = 7, // how often the plant should be watered, in days
     val imageUrl: String = ""
-) {
+) : Parcelable {
     override fun toString() = name
 }
