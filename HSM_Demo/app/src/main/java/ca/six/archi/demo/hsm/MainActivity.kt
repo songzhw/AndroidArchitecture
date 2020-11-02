9ec7b81eabc8206046575ee2a6445cce57a1155c
songzhw <songzhw2012@gmail.com>
2020-11-02 15:54:57 -0500

    crash: size = 0, index = 2

--- HSM_Demo/app/src/main/java/ca/six/archi/demo/hsm/MainActivity.kt
+++ HSM_Demo/app/src/main/java/ca/six/archi/demo/hsm/MainActivity.kt
@@ -1,19 +1,24 @@
 package ca.six.archi.demo.hsm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var parents: Array<IState> = arrayOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ary : Array<Int> = arrayOf()
+        ary.set(2, 20)
+        println("szw ary = $ary") //=> crash : ArrayIndexOutOfBoundsException: length=0; index=2
     }
 }
 
 interface IState {
-
+    val parents: Array<IState>
+        get() = arrayOf()
 }
 
-object LoggedOut : IState
-object LoggedIn: IState
-object LoggedIn_Abnormal: LoggedIn
+object LoggedOut: IState
