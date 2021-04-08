ad510bc5c655e5be8105f31e99ff5b7bb84faf29
songzhw <songzhw2012@gmail.com>
2021-04-08 17:56:21 -0400

    add http

---
+++ CleanViper/app/src/main/java/ca/six/demo/cleanviper/core/http/HttpService.kt
@@ -0,0 +1,10 @@
+package ca.six.demo.cleanviper.core.http
+
+import ca.six.demo.cleanviper.entity.BookChapter
+import io.reactivex.Observable
+import retrofit2.http.GET
+
+interface HttpService {
+    @GET("/dbfd6809-d551-4c23-94a5-696b3d4f7efd")
+    fun getChapters() : Observable<List<BookChapter>>
+}
