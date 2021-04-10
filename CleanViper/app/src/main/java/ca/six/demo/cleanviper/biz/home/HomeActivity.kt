4344810e8ff370fc9ff1860cb71d8b94af798a9c
songzhw <songzhw2012@gmail.com>
2021-04-10 10:57:11 -0400

    home ui

--- CleanViper/app/src/main/java/ca/six/demo/cleanviper/biz/home/HomeActivity.kt
+++ CleanViper/app/src/main/java/ca/six/demo/cleanviper/biz/home/HomeActivity.kt
@@ -9,19 +9,10 @@ import ca.six.demo.cleanviper.ext.clearedBy
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
-class HomeActivity : AppCompatActivity(R.layout.activity_main), HttpInjected, RxCleanerInjected {
+class HomeActivity : AppCompatActivity(R.layout.activity_home){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycle.addObserver(rxCleaner)
 
-        http.getChapters()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .flatMapIterable { resp -> resp.chapters }
-            .subscribe { cahpter ->
-                println("szw chapters = $cahpter")
-            }
-            .clearedBy(disposables)
     }
 }
