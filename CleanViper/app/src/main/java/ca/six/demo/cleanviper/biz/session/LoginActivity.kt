90fa7a80ca49d386f81bb7ba0551763ad1119d09
songzhw <songzhw2012@gmail.com>
2021-04-10 12:28:17 -0400

    full screen + noActionBar

--- CleanViper/app/src/main/java/ca/six/demo/cleanviper/biz/session/LoginActivity.kt
+++ CleanViper/app/src/main/java/ca/six/demo/cleanviper/biz/session/LoginActivity.kt
@@ -1,11 +1,17 @@
 package ca.six.demo.cleanviper.biz.session
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
 import androidx.appcompat.app.AppCompatActivity
 import ca.six.demo.cleanviper.R
 
-class LoginActivity : AppCompatActivity(R.layout.activity_login) {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_login) //上面两个得在加载layout文件之前
     }
 }
