98ea969dfaa79cdae2c59993d9b20ba512373e2f
songzhw <songzhw2012@gmail.com>
2021-04-13 18:25:37 -0400

    show progress

--- CleanViper/app/src/main/java/ca/six/demo/cleanviper/ext/ContextExt.kt
+++ CleanViper/app/src/main/java/ca/six/demo/cleanviper/ext/ContextExt.kt
@@ -2,19 +2,26 @@ package ca.six.demo.cleanviper.ext
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import android.view.Gravity
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.FrameLayout
 import android.widget.Toast
 import com.github.ybq.android.spinkit.SpinKitView
+import com.github.ybq.android.spinkit.style.CubeGrid
 
 fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) =
     Toast.makeText(this, message, duration).show()
 
-fun Activity.showProgress() {
+fun Activity.showProgress(): SpinKitView {
     val spinView = SpinKitView(this)
-    val lp = FrameLayout.LayoutParams(200, 200)
+    spinView.setColor(Color.MAGENTA)
+    spinView.setIndeterminateDrawable(CubeGrid())
+    val lp = FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
     lp.gravity = Gravity.CENTER
 
     val decorView = this.window.decorView as FrameLayout
     decorView.addView(spinView, lp)
+
+    return spinView
 }
