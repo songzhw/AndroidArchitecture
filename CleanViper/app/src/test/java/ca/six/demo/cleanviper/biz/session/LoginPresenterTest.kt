060fd308eef7b16c215d92d3ae04cf8fbb993f05
songzhw <songzhw2012@gmail.com>
2021-04-14 22:36:48 -0400

    fix the NPE error but the JVM does not RxAndroid -- Caused by: java.lang.RuntimeException: Method getMainLooper in android.os.Looper not mocked. See http://g.co/androidstudio/not-mocked for details.

--- CleanViper/app/src/test/java/ca/six/demo/cleanviper/biz/session/LoginPresenterTest.kt
+++ CleanViper/app/src/test/java/ca/six/demo/cleanviper/biz/session/LoginPresenterTest.kt
@@ -2,19 +2,22 @@ package ca.six.demo.cleanviper.biz.session
 
 import ca.six.demo.cleanviper.core.di.DepStore
 import ca.six.demo.cleanviper.core.http.HttpService
+import ca.six.demo.cleanviper.core.session.UserHttpResponse
+import io.reactivex.Observable
+import okhttp3.ResponseBody
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
+import retrofit2.Response
 
 class LoginPresenterTest {
-    lateinit var http: HttpService
+    lateinit var originalRetrofit: HttpService
     lateinit var view: ILoginView
 
     @Before
     fun before() {
-        http = DepStore.http
+        originalRetrofit = DepStore.http
         //注入mock
         val mockHttp = mock(HttpService::class.java)
         DepStore.http = mockHttp
@@ -25,13 +28,19 @@ class LoginPresenterTest {
     @After
     fun after() {
         // 还原
-        DepStore.http = http
+        DepStore.http = originalRetrofit
     }
 
 
     @Test
     fun testLogin_toastWhenLoginFailed() {
+        val body = ResponseBody.create(null, "")
+        val resp = Response.error<UserHttpResponse>(406, body)
+        `when`(DepStore.http.loginAndFail()).thenReturn(Observable.just(resp))
+
         val presenter = LoginPresenter(view)
+        presenter.login("", "")
+        verify(originalRetrofit).loginAndFail()
     }
 
     @Test
